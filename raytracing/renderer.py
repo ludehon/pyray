2718dea05531a8d098424def9a7a0d586feade7b
ludehon <luciendehon@gmail.com>
2019-08-28 17:20:52 +0200

    Added Renderer class, Started Vec3 tests

---
+++ raytracing/renderer.py
@@ -0,0 +1,9 @@
+
+
+class Renderer():
+    def __init__(self, world, resolution=(200, 200)):
+        self.world = world
+        self.resolution = resolution
+        
+    def render(self):
+        pass
